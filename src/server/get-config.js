9f7fb8fb575e933b886ec46a76029d457fb66857
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2021-07-29 15:29:24 +0300

    Fix docker file and workflows

--- src/server/get-config.js
+++ src/server/get-config.js
@@ -7,7 +7,7 @@ console.log(`eth-faucet - using faucet config at "${configPath}"`)
 const allConfigs = require(configPath)
 
 // load config for environment
-const environment = process.env.FAUCET_CONFIG || 'cesoie'
+const environment = process.env.FAUCET_CONFIG || 'ropsten'
 const config = allConfigs[environment]
 if (!config) throw new Error(`Unable to find config for environment "${environment}"`)
 if (!config.privateKey) throw new Error('No "privateKey" specified in config.')
